50dfc8aba3dbfdb7e514a20357e2895d341ad558
umairjutt381 <ch.umairjutt381@gmail.com>
2025-10-30 19:18:29 +0500

    fixes some issues

--- polls/urls.py
+++ polls/urls.py
@@ -5,8 +5,8 @@ app_name = "polls"
 urlpatterns = [
     path('', views.index_view, name='index'),
     path('add/', views.add_question, name='add_question'),
+    path('delete_selected/', views.delete_selected_polls, name='delete_selected'),
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
-    path("<int:question_id>/vote/results/", views.results, name="results"),
 ]
