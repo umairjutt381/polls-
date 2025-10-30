287f675a4a52e86218341e0d0fadd5a56cc58b5d
umairjutt381 <ch.umairjutt381@gmail.com>
2025-10-30 16:27:54 +0500

    fix and added api's

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from . import views
-
-# app_name = "polls"
+from polls import views
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index_view, name='index'),
+    path('add/', views.add_question, name='add_question'),
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:question_id>/vote/results/", views.results, name="results"),
 ]
